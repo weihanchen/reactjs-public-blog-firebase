610dc4c6b3a502fe5ad42878a6e03fa9c81b5029
weihanchen <willhanchen@gmail.com>
2016-10-31 23:36:29 +0800

    update home template

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import CSSModules from 'react-css-modules'
+import styles from './Home.css'
 
 class Home extends React.Component {
 	render() {
 		return (
-			<div>Home</div>
+			<div>
+			<p></p>
+			<div className="input-group">
+				<span className="input-group-addon">
+					<i className="fa fa-search fa fw"></i>
+				</span>
+				<input type="text" className="form-control" styleName="textbox-radius" placeholder="Search title..."/>
+			</div>
+			</div>
 		)
 	}
 }
--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp'),
 	clean = require('gulp-clean')
 
 gulp.task('css', function() {
-	return gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css']).pipe(gulp.dest('public/css/plugins'));
+	return gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css', 'bower_components/font-awesome/css/font-awesome.min.css']).pipe(gulp.dest('public/css/plugins'));
 });
 
 gulp.task('fonts', function() {
-	return gulp.src(['bower_components/bootstrap/dist/fonts/*']).pipe(gulp.dest('public/css/fonts'));
+	return gulp.src(['bower_components/bootstrap/dist/fonts/*', 'bower_components/font-awesome/fonts/*']).pipe(gulp.dest('public/css/fonts'));
 });
 
 gulp.task('cleanBowerFiles', ['css', 'fonts'], function() {
